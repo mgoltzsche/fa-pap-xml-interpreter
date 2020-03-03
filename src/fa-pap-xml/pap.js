228d98675d6650154a372c821d5002a13a0e37e5
Max Goltzsche <max.goltzsche@gmail.com>
2020-03-04 00:53:11 +0100

    Add input conversion.

--- src/fa-pap-xml/pap.js
+++ src/fa-pap-xml/pap.js
@@ -125,26 +125,34 @@ class PAP {
 		this.expr = new BlockExpression([...obj.methods, new ast.FunctionCallExpression(new ast.NameExpression('MAIN'), [])]);
 	}
 	inputs() {
-		return this.varDefs('INPUT');
+		return new VarDeclarations(this.varDecls('INPUT'));
+	}
+	defaults() {
+		let scope = newScope();
+		let interpreter = new PAPInterpreter(scope);
+		let varScope = {};
+		evaluateVars(this.vars, interpreter, varScope);
+		return varScope;
 	}
 	outputs() {
-		return this.varDefs('OUTPUT');
+		return new VarDeclarations(this.varDecls('OUTPUT'));
 	}
-	varDefs(type) {
+	varDecls(type) {
 		let matcher = new RegExp(`^${type}(:|$)`);
-		return this.vars.filter(v => v.varType.match(matcher)).map(v => {return {
-			name: v.name,
-			type: v.type,
-		};});
+		let v = this.vars.filter(v => v.varType.match(matcher)).map(v => new VarDeclaration(v.name, v.type, v.varType, v.comment));
+		if (v.length === 0) {
+			throw new Error(`No "${type}" var def found`);
+		}
+		return v;
 	}
 	evaluate(inputParams) {
 		let scope = newScope();
 		let interpreter = new PAPInterpreter(scope);
-		populateScope(scope, interpreter, this.vars);
+		evaluateVars(this.vars, interpreter, scope);
 		Object.assign(scope, inputParams);
 		this.expr.visit(interpreter);
 		let result = {};
-		for (let output of this.outputs()) {
+		for (let output of this.outputs().vars) {
 			let out = scope[output.name];
 			if (out === undefined) {
 				throw new Error(`Missing output ${output.name}`);
@@ -155,12 +163,59 @@ class PAP {
 	}
 }
 
-function populateScope(scope, interpreter, vars) {
-	for (let varDef of vars) {
-		if (scope[varDef.name] !== undefined) {
-			throw new Error(`Duplicate definition of PAP var ${varDef.name}`);
+function evaluateVars(vars, interpreter, scope) {
+	for (let v of vars) {
+		if (scope[v.name] !== undefined) {
+			throw new Error(`Duplicate definition of PAP var ${v.name}`);
 		}
-		scope[varDef.name] = varDef.evaluate(interpreter);
+		let value = v.evaluate(interpreter);
+		if (value !== undefined) {
+			scope[v.name] = value;
+		}
+	}
+}
+
+function validNumber(v) {
+	let n = new BigNumber(v);
+	if (n.isNaN() || !n.isFinite()) {
+		throw new Error(`Invalid number provided: ${v}`);
+	}
+	return n;
+}
+
+const valueConverters = {
+	BigDecimal: v => new BigDecimal(validNumber(v)),
+	int: v => validNumber(v),
+	double: v => validNumber(v),
+}
+
+class VarDeclaration {
+	constructor(name, type, varType, comment) {
+		this.name = name;
+		this.type = type;
+		this.varType = varType;
+		this.comment = comment;
+	}
+}
+
+class VarDeclarations {
+	constructor(varDecls) {
+		this.vars = varDecls;
+	}
+	createValues(input) {
+		let converted = {};
+		for (let v of this.vars) {
+			let inVal = input[v.name];
+			if (inVal === '' || inVal === undefined || inVal === null) {
+				throw new Error(`Undefined variable ${v.name}`);
+			}
+			let converter = valueConverters[v.type];
+			if (!converter) {
+				throw new Error(`No input converter registered for type ${v.type}`);
+			}
+			converted[v.name] = converter(inVal);
+		}
+		return converted;
 	}
 }
 
--- test/fa-pap-xml/pap-test.js
+++ test/fa-pap-xml/pap-test.js
@@ -4,36 +4,59 @@ import assert from 'assert';
 import BigNumber from 'bignumber.js';
 
 describe('FA PAP XML Interpreter', function() {
-	describe('load(xmlStr)', function() {
+	describe('load(xmlStr)...evaluate(input)', function() {
 		it('load', function() {
 			let Lohnsteuer2020 = fs.readFileSync('src/codes/Lohnsteuer2020.xml.xhtml', 'utf8');
+			
+			// Load PAP
 			let pap = load(Lohnsteuer2020);
 			assert(pap, 'pap');
 
+			// Assert inputs
 			let inputs = pap.inputs();
-			assert.ok(inputs.filter(n => n.name === 'KRV').length === 1, 'input var "KRV" should be among INPUT names');
-			assert.ok(inputs.filter(n => n.name === 'TAB1').length === 0, 'internal var "TAB1" should not be among INPUT names');
+			let inputVars = inputs.vars;
+			assert.ok(inputVars.filter(n => n.name === 'KRV').length === 1, 'input var "KRV" should be among INPUT names');
+			assert.ok(inputVars.filter(n => n.name === 'TAB1').length === 0, 'internal var "TAB1" should not be among INPUT names');
 
+			// Assert outputs
 			let outputs = pap.outputs();
-			assert.ok(outputs.filter(n => n.name === 'LSTLZZ').length === 1, 'output var "LSTLZZ" should be among OUTPUT names');
-			assert.ok(outputs.filter(n => n.name === 'VFRB').length === 1, 'output var "VFRB" should be among OUTPUT names');
-			assert.ok(outputs.filter(n => n.name === 'KRV').length === 0, 'INPUT var "KRV" should not be among OUTPUT names');
+			let outputVars = outputs.vars;
+			assert.ok(outputVars.filter(n => n.name === 'LSTLZZ').length === 1, 'output var "LSTLZZ" should be among OUTPUT names');
+			assert.ok(outputVars.filter(n => n.name === 'VFRB').length === 1, 'output var "VFRB" should be among OUTPUT names');
+			assert.ok(outputVars.filter(n => n.name === 'KRV').length === 0, 'INPUT var "KRV" should not be among OUTPUT names');
 
-			let inputTypeMap = {
-				BigDecimal: i => new BigDecimal(new BigNumber(i + 100)),
-				int: i => new BigNumber(i + 10),
-				double: i => new BigNumber(i + 0.5),
+			// Assert input defaults
+			let defaults = pap.defaults();
+			assert(Object.keys(defaults).length > 0, '#defaults()');
+
+			// Assert input conversion
+			let rawInputValues = {};
+			for (let i = 0; i < inputVars.length; i++) {
+				rawInputValues[inputVars[i].name] = i + 100;
 			}
-			let inputParams = inputs.reduce((r,n,i) => {
-				let inputGen = inputTypeMap[n.type];
-				if (!inputGen) {
-					throw new Error(`Unsupported var type ${n.type}`);
-				}
-				r[n.name] = inputGen(i);
-				return r;
-			}, {});
-			let evaluated = pap.evaluate(inputParams);
+			let inputValues = inputs.createValues(rawInputValues);
+			let expectedInputNames = inputVars.map(v => v.name);
+			let actualInputNames = Object.keys(inputValues);
+			expectedInputNames.sort();
+			actualInputNames.sort();
+			assert.equal(expectedInputNames.join(','), actualInputNames.join(','), 'converted input names names');
+			for (let [k,v] of Object.entries(inputValues)) {
+				assert.equal(''+rawInputValues[k], ''+v, `#createValues()[${k}]`);
+			}
+
+			// Evaluate
+			let evaluated = pap.evaluate(inputValues);
 			assert(evaluated, 'evaluated');
+
+			// Assert actual outputs are not empty and names equal expected
+			let expectedOutputNames = outputVars.map(i => i.name);
+			let actualOutputNames = Object.keys(evaluated);
+			expectedOutputNames.sort();
+			actualOutputNames.sort();
+			assert.equal(expectedOutputNames.join(','), actualOutputNames.join(','), 'output names');
+			for (let [k,v] of Object.entries(evaluated)) {
+				assert(v, `output ${k} should not be empty`);
+			}
 		});
 	});
 });
